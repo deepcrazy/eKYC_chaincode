1e31965016a70a210596c592ff941ac42e5e1f46
deepcrazy <312.gupta@gmail.com>
2020-03-08 15:21:09 -0400

    Added comments and cleaned code for improvised readability

--- javascript/lib/fabcar.js
+++ javascript/lib/fabcar.js
@@ -10,6 +10,7 @@ class FabCar extends Contract {
 
     async initLedger(ctx) {
         console.info('============= START : Initialize Ledger ===========');
+        // Adding users for Testing purpose
         const users = [
             {
                 firstName: 'Alice',
@@ -26,74 +27,6 @@ class FabCar extends Contract {
                 passport: 832654,
             },
         ];
-        // const cars = [
-        //     {
-        //         color: 'blue',
-        //         make: 'Toyota',
-        //         model: 'Prius',
-        //         owner: 'Tomoko',
-        //     },
-        //     {
-        //         color: 'red',
-        //         make: 'Ford',
-        //         model: 'Mustang',
-        //         owner: 'Brad',
-        //     },
-        //     {
-        //         color: 'green',
-        //         make: 'Hyundai',
-        //         model: 'Tucson',
-        //         owner: 'Jin Soo',
-        //     },
-        //     {
-        //         color: 'yellow',
-        //         make: 'Volkswagen',
-        //         model: 'Passat',
-        //         owner: 'Max',
-        //     },
-        //     {
-        //         color: 'black',
-        //         make: 'Tesla',
-        //         model: 'S',
-        //         owner: 'Adriana',
-        //     },
-        //     {
-        //         color: 'purple',
-        //         make: 'Peugeot',
-        //         model: '205',
-        //         owner: 'Michel',
-        //     },
-        //     {
-        //         color: 'white',
-        //         make: 'Chery',
-        //         model: 'S22L',
-        //         owner: 'Aarav',
-        //     },
-        //     {
-        //         color: 'violet',
-        //         make: 'Fiat',
-        //         model: 'Punto',
-        //         owner: 'Pari',
-        //     },
-        //     {
-        //         color: 'indigo',
-        //         make: 'Tata',
-        //         model: 'Nano',
-        //         owner: 'Valeria',
-        //     },
-        //     {
-        //         color: 'brown',
-        //         make: 'Holden',
-        //         model: 'Barina',
-        //         owner: 'Shotaro',
-        //     },
-        //     {
-        //         color: 'green',
-        //         make: 'Audi',
-        //         model: 'A4',
-        //         owner: 'Deepanshu',
-        //     },
-        // ];
 
         for (let i = 0; i < users.length; i++) {
             users[i].docType = 'user';
@@ -104,6 +37,7 @@ class FabCar extends Contract {
         console.info('============= END : Initialize Ledger ===========');
     }
 
+    //  Fetches individual's data for KYC information
     async getData(ctx, userId) {
         const userAsBytes = await ctx.stub.getState(userId);    //  get the user from the chaincode state
         if (!userAsBytes || userAsBytes.length === 0) {
@@ -113,96 +47,73 @@ class FabCar extends Contract {
         return userAsBytes.toString();
     }
 
-    // async queryCar(ctx, carNumber) {
-    //     const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
-    //     if (!carAsBytes || carAsBytes.length === 0) {
-    //         throw new Error(`${carNumber} does not exist`);
-    //     }
-    //     console.log(carAsBytes.toString());
-    //     return carAsBytes.toString();
-    // }
-
+    //  store user data like firstName, lastName, DOB, income, passport
     async inputData(ctx, userId, userDetails) {
         console.log('============= START : Save User Data =========');
-        // users.push({userId : user});
+
         const user = {
             docType: 'user',
             userDetails,
         };
-        await ctx.stub.putState(userId, Buffer.from(JSON.stringify(user)));
+
+        await ctx.stub.putState(userId, Buffer.from(JSON.stringify(user))); //  store the user details into the blockchain
         console.log('======== END : User Data Stored ===========');
     }
 
+    //  users approves companies to access their information
     async approveCompany(ctx, userId, companyId) {
         console.log('======== START : Approve company for user data access ==========');
-        let relations = 'id1~id2';
-        let companyUserIndexKey = await ctx.stub.createCompositeKey(relations, [companyId.toString(), userId.toString()]);
-        let userCompanyIndexKey = await ctx.stub.createCompositeKey(relations, [userId.toString(), companyId.toString()]);
+
+        //  `relations` created for 2-way access like if company id is given then list of users will be fetched  and vice versa too.
+        let relations = 'id1~id2';  //  relation type to be stored on blockchain (company~user or user~company)
+        let companyUserIndexKey = await ctx.stub.createCompositeKey(relations, [companyId.toString(), userId.toString()]);  //  create company~user relation unique key
+        let userCompanyIndexKey = await ctx.stub.createCompositeKey(relations, [userId.toString(), companyId.toString()]);  //  create user~company relation unique key
+
+        // Validations for composite keys created
         if (!companyUserIndexKey) {
-            throw new Error(' Failed to create the company~user createCompositeKey');
+            throw new Error('Composite key: companyUserIndexKey is null');
         }
 
         if (!userCompanyIndexKey) {
-            throw new Error(' Failed to create the user~company createCompositeKey');
+            throw new Error('Composite key: userCompanyIndexKey is null');
         }
 
         console.log(companyUserIndexKey);
 
         //  Note - passing a 'nil' value will effectively delete the key from state, therefore we pass null character as value
-        await ctx.stub.putState(companyUserIndexKey, Buffer.from('\u0000'));
-        await ctx.stub.putState(userCompanyIndexKey, Buffer.from('\u0000'));
+        await ctx.stub.putState(companyUserIndexKey, Buffer.from('\u0000'));    //  Store company~user unique key relation
+        await ctx.stub.putState(userCompanyIndexKey, Buffer.from('\u0000'));    //  Store user~company unique key relation
         console.log('======== END : Relation of approved companies for users stored =========');
     }
 
+    //  Used to save companyId to list of company ids
     async saveCompany(ctx, companyID) {
         console.log('=========== START : Save Company Data ===========');
 
-        const companies_list_id = 'companies_list_id';
-        // const companyId = {
-        //     docType: 'company',
-        //     companyID,
-        // };
+        const companies_list_id = 'companies_list_id';  //  Static id for accessing the list of companies
 
-        const companiesAsBytes = await ctx.stub.getState(companies_list_id);    //  get the user from the chaincode state
+        const companiesAsBytes = await ctx.stub.getState(companies_list_id);    //  get the companies list from the blockchain
         if (!companiesAsBytes || companiesAsBytes.length === 0) {
-            const companyObj = {list: [companyID]};
-            console.log(`check: ${JSON.stringify(companyObj)}`);
-            await ctx.stub.putState(companies_list_id, Buffer.from(JSON.stringify(companyObj)));
+            const companyObj = {list: [companyID]}; //  create the companyObj for storing the list of companies
+            console.log(`CompanyObj: ${JSON.stringify(companyObj)}`);
+            await ctx.stub.putState(companies_list_id, Buffer.from(JSON.stringify(companyObj)));    //  Store the newly created companies list into the blockchain
             return;
         }
 
-        console.log(`Companies as bytes: ${companiesAsBytes.toString()}`);
+        console.log(`Fetched CompanyObj from blockchain: ${companiesAsBytes.toString()}`);
         let companyObj = JSON.parse(companiesAsBytes);
 
-        console.log(`Companies Obj: ${companyObj}`);
-        // return userAsBytes.toString();
+        companyObj.list.push(companyID);    //  Adding the companyID into the existing list of the companies list
+        console.log(`Updated companyObj: ${companyObj}`);
 
-        companyObj.list.push(companyID);
-        console.log(`Updated Companies Obj: ${companyObj}`);
-
-        // await ctx.stub.putState(companyId, Buffer.from(JSON.stringify(company)));
-        await ctx.stub.putState(companies_list_id, Buffer.from(JSON.stringify(companyObj)));
+        await ctx.stub.putState(companies_list_id, Buffer.from(JSON.stringify(companyObj)));    //  Store the updated companies list into the blochchain
         console.log('======== END : Company Id Stored ==========');
     }
 
-    // async createCar(ctx, carNumber, make, model, color, owner) {
-    //     console.info('============= START : Create Car ===========');
-
-    //     const car = {
-    //         color,
-    //         docType: 'car',
-    //         make,
-    //         model,
-    //         owner,
-    //     };
-
-    //     await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
-    //     console.info('============= END : Create Car ===========');
-    // }
-
+    //  Fetches a list of all approved and registered companies
     async getCompanies(ctx) {
 
-        const companies_list_id = 'companies_list_id';
+        const companies_list_id = 'companies_list_id';  //  Static id for accessing the list of companies
         const companiesAsBytes = await ctx.stub.getState(companies_list_id);    //  get the companies list from the chaincode state
         if (!companiesAsBytes || companiesAsBytes.length === 0) {
             throw new Error('Companies list is empty.');
@@ -211,86 +122,42 @@ class FabCar extends Contract {
         return JSON.parse(companiesAsBytes);
     }
 
+    //  for company - function to get approved userIds and for user - function to get approved companyIds
     async getRelations(ctx, id) {
 
-        let relations = 'id1~id2';
-        let relationResultsIterator = await ctx.stub.getStateByPartialCompositeKey(relations, [id.toString()]);
+        //  `relations` created for 2-way access like if company id is given then list of users will be fetched  and vice versa too.
+        let relations = 'id1~id2';  //  relation type to be stored on blockchain (company~user or user~company)
+        let relationResultsIterator = await ctx.stub.getStateByPartialCompositeKey(relations, [id.toString()]); //  Get company~user or user~company number of relations
 
-        let relationsArray = [];
+        let relationsArray = [];    //  To store list of company Ids if userId is passed as input in function and to store list of userIds if compnayId is passed as input in function
         while (true) {
-            // let Record;
+
             let responseRange = await relationResultsIterator.next();
+
+            //  Validation if list of userIds or companyIds is empty
             if (!responseRange || !responseRange.value || !responseRange.value.key) {
                 console.log('end of data');
                 console.log(`relationsArray: ${relationsArray}`);
                 return JSON.stringify(relationsArray);
             }
 
-            console.log(`response range: ${responseRange.value.key}`);
-            console.log(`response range string: ${responseRange.value.key.toString('utf8')}`);
+            console.log(`Response value: ${responseRange.value.key.toString('utf8')}`);
             let objectType;
             let attributes;
+            //  Split the composite key to get the companyIds and userIds
             ({
                 objectType,
                 attributes
             } = await ctx.stub.splitCompositeKey(responseRange.value.key));
 
             // let id1 = attributes[0];   //  ID1 param of the relation
-            let id2 = attributes[1];         //  ID2 param of the relation
+            let id2 = attributes[1];         //  ID2 param of the relation (ID2 = userId if attributes[0] = companyId or ID2 = companyId if attributes[0] = userID)
             console.log(`Index Type: ${objectType}`);
-            // Record = responseRange.value.key.toString('utf8');
-            relationsArray.push(id2);
+
+            relationsArray.push(id2);   //  Adding the list of userIds if filtered on the basis of companyIds or vice versa.
 
         }
-
     }
-
-    // async queryAllCars(ctx) {
-    //     const startKey = 'CAR0';
-    //     const endKey = 'CAR999';
-
-    //     const iterator = await ctx.stub.getStateByRange(startKey, endKey);
-
-    //     const allResults = [];
-    //     while (true) {
-    //         const res = await iterator.next();
-
-    //         if (res.value && res.value.value.toString()) {
-    //             console.log(res.value.value.toString('utf8'));
-
-    //             const Key = res.value.key;
-    //             let Record;
-    //             try {
-    //                 Record = JSON.parse(res.value.value.toString('utf8'));
-    //             } catch (err) {
-    //                 console.log(err);
-    //                 Record = res.value.value.toString('utf8');
-    //             }
-    //             allResults.push({ Key, Record });
-    //         }
-    //         if (res.done) {
-    //             console.log('end of data');
-    //             await iterator.close();
-    //             console.info(allResults);
-    //             return JSON.stringify(allResults);
-    //         }
-    //     }
-    // }
-
-    // async changeCarOwner(ctx, carNumber, newOwner) {
-    //     console.info('============= START : changeCarOwner ===========');
-
-    //     const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
-    //     if (!carAsBytes || carAsBytes.length === 0) {
-    //         throw new Error(`${carNumber} does not exist`);
-    //     }
-    //     const car = JSON.parse(carAsBytes.toString());
-    //     car.owner = newOwner;
-
-    //     await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
-    //     console.info('============= END : changeCarOwner ===========');
-    // }
-
 }
 
 module.exports = FabCar;
